0a43c3f87becdb756b58b2ce3141efb273220c81
mancuoj <1337381768@qq.com>
2022-04-05 11:36:11 +0800

    chore: init

---
+++ match_client/client.py
@@ -0,0 +1,34 @@
+from match import Match
+from match.ttypes import User
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+
+def main():
+    # Make socket
+    transport = TSocket.TSocket('localhost', 9090)
+
+    # Buffering is critical. Raw sockets are very slow
+    transport = TTransport.TBufferedTransport(transport)
+
+    # Wrap in a protocol
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    # Create a client to use the protocol encoder
+    client = Match.Client(protocol)
+
+    # Connect!
+    transport.open()
+
+    # 测试
+    user = User(1, "mancuoj", 1500)
+    client.add_user(user, "")
+
+    # Close!
+    transport.close()
+
+if __name__=="__main__":
+    main()
