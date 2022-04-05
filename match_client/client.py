5e28f69ab32b9e84ce9ae1e1825e9fcb9a0bf8f6
mancuoj <1337381768@qq.com>
2022-04-05 22:32:31 +0800

    feat: complete match client

--- match_client/client.py
+++ match_client/client.py
@@ -6,29 +6,30 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
-    # Make socket
+
+def operate(op, user_id, username, score):
     transport = TSocket.TSocket('localhost', 9090)
-
-    # Buffering is critical. Raw sockets are very slow
     transport = TTransport.TBufferedTransport(transport)
-
-    # Wrap in a protocol
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-    # Create a client to use the protocol encoder
     client = Match.Client(protocol)
-
-    # Connect!
     transport.open()
 
-    # 测试
-    user = User(1, "mancuoj", 1500)
-    client.add_user(user, "")
+    user = User(user_id, username, score)
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
 
-    # Close!
     transport.close()
 
-if __name__=="__main__":
+
+def main():
+    for line in stdin:
+        op, user_id, username, score = line.split(" ")
+        operate(op, int(user_id), username, int(score))
+
+
+if __name__ == "__main__":
     main()
