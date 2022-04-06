8b1c74679bf88bcacb7870df0359b2fac9eae2db
mancuoj <1337381768@qq.com>
2022-04-06 10:39:23 +0800

    feat: improve matching mechanism

--- match_server/main.cpp
+++ match_server/main.cpp
@@ -13,34 +13,116 @@ using namespace ::apache::thrift::server;
 
 using namespace ::match_service;
 
-class MatchHandler : virtual public MatchIf {
- public:
-  MatchHandler() {
-  }
+using namespace std;
 
-  int32_t add_user(const User& user, const std::string& info) {
-    printf("add_user\n");
-    return 0;
-  }
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <vector>
 
-  int32_t remove_user(const User& user, const std::string& info) {
-    printf("remove_user\n");
-    return 0;
-  }
+struct Task {
+    User user;
+    string type;
 };
 
+struct MsgQueue {
+    queue<Task> q;
+    mutex m;
+    condition_variable cv;
+}msg_queue;
+
+class Pool {
+    public:
+        void save_result(int a, int b) {
+            printf("Match Result: %d %d\n", a, b);
+        }
+
+        void match() {
+            while (users.size() > 1) {
+                auto a = users[0], b = users[1];
+                users.erase(users.begin());
+                users.erase(users.begin());
+                save_result(a.id, b.id);
+            }
+        }
+
+        void add(User user) {
+            users.push_back(user);
+        }
+
+        void remove(User user) {
+            for (uint32_t i = 0; i < users.size(); i++){
+                if (users[i].id == user.id) {
+                    users.erase(users.begin()+i);
+                    break;
+                }
+            }
+        }
+    private:
+        vector<User> users;
+}pool;
+
+class MatchHandler : virtual public MatchIf {
+    public:
+        MatchHandler() {
+        }
+
+        int32_t add_user(const User& user, const std::string& info) {
+            printf("add_user\n");
+
+            unique_lock<mutex> lck(msg_queue.m);
+            msg_queue.q.push({user, "add"});
+            msg_queue.cv.notify_all();
+
+            return 0;
+        }
+
+        int32_t remove_user(const User& user, const std::string& info) {
+            printf("remove_user\n");
+
+            unique_lock<mutex> lck(msg_queue.m);
+            msg_queue.q.push({user, "remove"});
+            msg_queue.cv.notify_all();
+
+            return 0;
+        }
+};
+
+
+void consume_task() {
+    while(true) {
+        unique_lock<mutex> lck(msg_queue.m);
+        if (msg_queue.q.empty()) {
+            msg_queue.cv.wait(lck);
+        } else {
+            auto task = msg_queue.q.front();
+            msg_queue.q.pop();
+            lck.unlock();
+            
+            if (task.type == "add") pool.add(task.user);
+            else if (task.type == "remove") pool.remove(task.user);
+
+            pool.match();
+        }
+    }
+}
+
 int main(int argc, char **argv) {
-  int port = 9090;
-  ::std::shared_ptr<MatchHandler> handler(new MatchHandler());
-  ::std::shared_ptr<TProcessor> processor(new MatchProcessor(handler));
-  ::std::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  ::std::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  ::std::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+    int port = 9090;
+    ::std::shared_ptr<MatchHandler> handler(new MatchHandler());
+    ::std::shared_ptr<TProcessor> processor(new MatchProcessor(handler));
+    ::std::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+    ::std::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+    ::std::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
-  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+    TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
 
-  std::cout << "Start match server 😎" << std::endl;
-  server.serve();
-  return 0;
+    cout << "Start match server 😎" << endl;
+    thread matching_thread(consume_task);
+
+    server.serve();
+    return 0;
 }
 
